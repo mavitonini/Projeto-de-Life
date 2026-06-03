f3140a328b553161cc3c18b5aa41b0d8f3459286
mavitonini <maria.tonini@escola.pr.gov.br>
2026-06-03 12:42:28 +0000

    676767676767676767

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-12-30T00:00:00");
-const tempoObjetivo2 = new Date("2026-12-18T00:00:00");
-const tempoObjetivo3 = new Date("2026-12-18T00:00:00");
-const tempoObjetivo4 = new Date("2109-07-15T00:00:00");
+const tempoObjetivo1 = new Date("2026-12-23T00:00:00");
+const tempoObjetivo2 = new Date("2026-10-20T00:00:00");
+const tempoObjetivo3 = new Date("2026-11-08T00:00:00");
+const tempoObjetivo4 = new Date("2026-08-09T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
